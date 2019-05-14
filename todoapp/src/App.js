4dd24ab88f5c36e3814d49f3f496977a6edc1e2f
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-05-13 23:21:48 -0400

    delete tasks

---
+++ todoapp/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Todos from './Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'Freecodecamp ES6 section' },
+      { id: 2, content: 'Freecodecamp Object Oriented Programming' }
+    ]
+  };
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    });
+  };
+
+  render() {
+    return (
+      <div className='todo-app container'>
+        <h1 className='center blue-text'>Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
+}
+export default App;
